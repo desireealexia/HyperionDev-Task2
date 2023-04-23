02ceca4dabcba4980a629a6dacf34e181f47f08d
Desiree Williams-Forde <130295888+desireealexia@users.noreply.github.com>
2023-04-23 14:24:16 +0100

    Add files via upload

---
+++ replace.py
@@ -0,0 +1,10 @@
+sentence = "The!quick!brown!fox!jumps!over!the!lazy!dog."
+
+sentence = sentence.replace("!" , " ")
+print(sentence)
+
+sentence = sentence.upper()
+print(sentence)
+
+sentence = sentence[::-1]
+print(sentence)
